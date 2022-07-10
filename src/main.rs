07a4d180b026e65ed1e4da971275281855e64ecc
Ville-Matti Hakanpää <villematti@villematti.info>
2022-07-10 23:52:21 +0300

    Add Guess stuct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ use std::io;
 use std::cmp::Ordering;
 use rand::{self, Rng};
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess must be between 1 adn 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -21,10 +39,12 @@ fn main() {
                 Ok(num) => num,
                 Err(_) => continue,
             };
+        
+        let guess = Guess::new(guess);
 
-        println!("You guessed: {guess}");
+        println!("You guessed: {}", guess.value);
 
-        match guess.cmp(&secret_number) {
+        match guess.value.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
